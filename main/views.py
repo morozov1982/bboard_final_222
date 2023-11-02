510f7439fc093a0fe9bedc435bbe8c4bcb326b09
Alexandr Morozov <morozov1982@gmail.com>
2023-11-02 17:14:36 +0600

    lesson_57 files

--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template.loader import get_template
 from django.template.exceptions import TemplateDoesNotExist
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm
 
 
 def index(request):
@@ -25,3 +32,30 @@ class BbLoginView(LoginView):
 @login_required
 def profile(request):
     return render(request, 'main/profile.html')
+
+
+class BbLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
+
+class BbPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/password_change.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя изменён'
