7545082e44fc71a6edcf89b11de4324220693e9a
Alexandr Morozov <morozov1982@gmail.com>
2023-11-13 17:12:17 +0600

    lesson_61 end_files

--- bboard_final/settings.py
+++ bboard_final/settings.py
@@ -42,13 +42,17 @@ INSTALLED_APPS = [
     'django_cleanup',
     'easy_thumbnails',
     'captcha',
+    'rest_framework',
+    'corsheaders',
 
     'main.apps.MainConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -152,3 +156,5 @@ THUMBNAIL_ALIASES = {
 }
 THUMBNAIL_BASEDIR = 'thumbnails'
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
--- bboard_final/urls.py
+++ bboard_final/urls.py
@@ -8,7 +8,8 @@ from django.views.decorators.cache import never_cache
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('captcha/', include('captcha.urls')),
-    path('', include('main.urls'))
+    path('api/', include('api.urls')),
+    path('', include('main.urls')),
 ]
 
 if settings.DEBUG:
